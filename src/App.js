9e75427c0fb7d0b7d6c668b2de6650a22838d664
yelim1215 <yerim1215jg@naver.com>
2023-03-31 11:07:27 +0900

    9. React에서 리스트 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import Todo from './Todo';
+import {Paper, List} from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      item :{id: 0, title: "Hello World 1", done: true},
+  constructor(props){ // 매개변수 props 생성자
+    super(props); // 매개변수 props 초기화
+    this.state = { // item에 item.id, item.title, item.done 매개변수 이름과 값 할당
+      item :[
+        {id: 0, title: "Hello World 1", done: false},
+        {id: 1, title: "Hello World 2", done: false},
+      ],
     };
   }
     render(){
-        return (
-            <div className="App">
-                <Todo item={this.state.item} />
-            </div>
-        );
+      // 자바스크립트가 제공하는 map 함수를 이용해서 배열을 반복해 <Todo /> 컴포넌트를 여러개 생성한다.
+      var todoItems = this.state.items.map((item, idx) => (
+        <Todo item = {item} key = {item.id} />
+      ));
+
+      // 생성된 컴포넌트 JSX를 리턴한다.
+        return <div className="App">{todoItems}</div>;
     }
 }
 
--- src/Todo.js
+++ src/Todo.js
@@ -3,16 +3,17 @@ import React from "react";
 class Todo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {item: props.item};
+        this.state = {item: props.item}; // 매개변수 item의 변수 값을 item에 대입
     }
+
     render() {
         return (
             <div className="Todo">
                 <input
                     type = "checkbox"
-                    id = {this.state.item.id}
-                    name = {this.state.item.done}
-                    checked = {this.state.item.done}
+                    id = {this.state.item.id} // item.id 값으로 렌더링하란 의미(JPX)
+                    name = {this.state.item.id}
+                    checked = {this.state.item.done} // item.done 값으로 렌더링하란 의미
                 />
                 <label for = {this.state.item.id}> {this.state.item.title}</label>
             </div>
