c0d654de45adfbfb7af34cbef1ea1547e606dede
yelim1215 <yerim1215jg@naver.com>
2023-03-31 10:46:05 +0900

    8. Props, state 사용하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Todo from './Todo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      item :{id: 0, title: "Hello World 1", done: true},
+    };
+  }
+    render(){
+        return (
+            <div className="App">
+                <Todo item={this.state.item} />
+            </div>
+        );
+    }
 }
 
 export default App;
