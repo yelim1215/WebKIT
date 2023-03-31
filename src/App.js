b1ab0cbae71861d3c42f0b27cd86d6ca0673ea15
yelim1215 <yerim1215jg@naver.com>
2023-03-31 14:04:50 +0900

    10. React 이벤트 핸들러로 목록 추가하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Todo from './Todo';
-import {Paper, List} from "@material-ui/core";
+import AddTodo from './AddTodo';
+import {Paper, List, Container} from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
@@ -13,6 +14,16 @@ class App extends React.Component {
       ],
     };
   }
+  // (1) add 함수 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; // key를 위한 id 추가
+    item.done = false;
+    thisItems.push(item);
+    this.setState({items: thisItems}); // update state
+    console.log("items: ", this.state.items);
+  }
+
   render(){
     // todoItems에 this.state.items.length 가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다.
     // todoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
@@ -26,8 +37,15 @@ class App extends React.Component {
       </Paper>
     );
 
-    // 생성된 컴포넌트 JSX를 리턴한다.
-      return <div className="App">{todoItems}</div>;
+    // (2) add 함수 연결
+    return (
+      <div className='App'>
+        <Container maxWidth="md">
+          <AddTodo add = {this.add} />
+          <div className = "TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
   }
 }
 
